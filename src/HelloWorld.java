802c21c570fcc68da72eb7bc2b0cc86013b5843d
mikehopps <mhopps@gmail.com>
2017-09-07 13:18:13 -0400

    Sept 7 - int double - data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,35 +7,42 @@ public class HelloWorld {
     //main method - this is the executed chunk of code
     //psvm TAB (ENTER)
     public static void main(String[] args) {
-        //this is the print command!
-//        System.out.println("5" + "4"); //sout TAB (ENTER)
-//        //"5" string not a number
-//        System.out.println(5 + -4);
-//        //int -> integer!
-//        System.out.println(72.41 + 0.5);
 
-        //Variables!
-        //1. The first time a var is used...
-            //you need to DECLARE the var type...
-        int age;  //declaration
-        age = 36; //assignment (right to left)
-        int heightInches = 72;
-        //increase age by 1.  Happy birthday!
-        age = age + 1;
+//        //Data Types!
+//        //String - collection of characters in quotes!
+//        System.out.println("Hello World!"); //sout TAB
+//        //int - positive, negative whole #'s and 0
+//        System.out.println(5);
+//        System.out.println(4 + 7);
+//
+//        System.out.println("4" + "7");
+//        //+ operator with strings! concatenation - sticks like glue
+//
+//        System.out.println(6.2 + 10.0/2.0);
+
+        //variables!
+        //1. Declare the var (age) and its type (int)
+        //2. Assign the value (36)
+        int age = 17;
+        int heightInches;  //declare heightInches
+        heightInches = 72; //assignment
+        System.out.println(age);
+        System.out.println(heightInches);
+        //happy birthday! one year older!
+        age = age + 1; //assignment goes right to left
+        //age + 1 = age;  //NOPE
         System.out.println(age);
 
-        double price = 20; //double can hold an int...
-        //make a var for pricePlusTax...tax is 6.25%
-        double pricePlusTax = price * 1.0625;
-        System.out.println(pricePlusTax);
-//        age = price; //ints cannot hold doubles.
-        System.out.println(7.001 + 2.1333);
-        System.out.println(13.49 - .63);
+        //make a variable that is called price and holds the value 19.99
+        double price = 19.99;
+        //change price to reflect the 6.25% MA sales tax
+        price = price * 1.0625;
+        System.out.println(price); //21.239375
+
+        //age = price;  //int cannot hold on to a double!
+        price = age;
+        System.out.println(33.95 - 71.047802);
 
     }
 }
-/*
-This is a multiline comment!
-You can make as many lines as you want in here.
-So.  Cool.
-*/
+
